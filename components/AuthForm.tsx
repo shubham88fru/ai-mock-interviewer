105f23e628d6cca2b03f653533176c0dd8804d41
shubham88fru <shubham88fru@yahoo.com>
2025-04-05 17:39:39 -0700

    cleanup

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -4,14 +4,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { Button } from "@/components/ui/button";
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
+import { Form } from "@/components/ui/form";
 import Image from "next/image";
 import Link from "next/link";
 import { toast } from "sonner";
